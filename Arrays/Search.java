f2937de873b3f98158d46a4f4af740a4adedb3e4
Chetan Umesh Ingole <98551496+chetan-i9@users.noreply.github.com>
2022-08-03 02:17:11 +0530

    Update Search.java

--- Arrays/Search.java
+++ Arrays/Search.java
@@ -1,4 +1,42 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Search {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the number of elements in an array: ");
+        int n=input.nextInt();
+
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            System.out.print("Enter element "+(i+1)+": ");
+            arr[i]=input.nextInt();
+        }
+
+        System.out.print("Enter the element to be searched in an array: ");
+        int num=input.nextInt();
+        int res=search(arr,num);
+        if (res==-1)
+        {
+            System.out.println("The element is not present in the array.");
+        }
+        else {
+            System.out.println("The element "+arr[res]+" is present at position "+(res+1));
+        }
+
+    }
+
+    private static int search(int[] arr,int num)
+    {
+        for (int i=0;i<arr.length;i++)
+        {
+            if (arr[i]==num)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
